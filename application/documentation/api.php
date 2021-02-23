dae81ff07b8413e58bbf3c60a224fd8817ef2610
tadson10 <ts6103@student.uni-lj.si>
2021-02-23 21:10:14 +0100

    Cors problem fixing

--- application/documentation/api.php
+++ application/documentation/api.php
@@ -1,6 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+
 require("../../vendor/autoload.php");
 $openapi = \OpenApi\scan("../controllers");
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
 echo $openapi->toJson();
